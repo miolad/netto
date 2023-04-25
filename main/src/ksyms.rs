52b8e27c02874728d9b83034101c55555ed7c771
Davide Miola <davide.miola99@gmail.com>
2023-04-25 09:19:52 +0000

    Rework symbol matching to be more efficient

--- main/src/ksyms.rs
+++ main/src/ksyms.rs
@@ -1,26 +1,16 @@
-use std::{io::{self, BufReader, BufRead}, fs::File, ops::{Range, Add, AddAssign}, collections::BTreeSet, iter::Sum};
+use std::{io::{self, BufReader, BufRead}, fs::File, ops::{Add, AddAssign}, collections::BTreeMap, iter::Sum};
 
 /// Helper to load and manage application-defined kernel symbols
 #[derive(Default)]
 pub struct KSyms {
-    net_rx_action: Range<u64>,
-    __napi_poll: Range<u64>,
-    netif_receive_skb: Range<u64>,
-    netif_receive_skb_core: Range<u64>,
-    netif_receive_skb_list_internal: Range<u64>,
-    napi_gro_receive: Range<u64>,
-    do_xdp_generic: Range<u64>,
-    tcf_classify: Range<u64>,
-    br_handle_frame: Range<u64>,
-    ip_forward: Range<u64>,
-    ip6_forward: Range<u64>,
-    ip_local_deliver: Range<u64>,
-    ip6_input: Range<u64>,
-    ip_rcv: Range<u64>,
-    ipv6_rcv: Range<u64>,
-    ip_rcv_finish: Range<u64>,
-    ip6_rcv_finish: Range<u64>,
-    nf_hook_slow: Range<u64>
+    syms: BTreeMap<u64, KSymsVal>
+}
+
+type SymbolFun = Box<dyn for<'a> Fn(&'a mut Counts, &'a mut u16, &'a mut u16) -> Option<&'a mut u16>>;
+
+struct KSymsVal {
+    range_end: u64,
+    fun: SymbolFun
 }
 
 /// Counts instances of symbols in stack traces
@@ -49,26 +39,9 @@ pub struct Counts {
 }
 
 impl KSyms {
-    fn empty() -> Self {
-        Default::default()
-    }
-
-    fn find_range_end(range: &mut Range<u64>, addrs: &BTreeSet<u64>) {
-        // TODO: remove me!
-        // Checks if the symbol was found
-        assert_ne!(range.start, 0);
-        
-        range.end = addrs
-            .range(range.start+1..)
-            .next()
-            .cloned()
-            .unwrap_or(range.start + 1);
-    }
-    
     /// Load requested kernel symbols from /proc/kallsyms
     pub fn load() -> io::Result<Self> {
-        let mut syms = Self::empty();
-        let mut btree = BTreeSet::new();
+        let mut btree = BTreeMap::new();
         let f = BufReader::new(File::open("/proc/kallsyms")?);
         
         // Load all the addresses into a BTreeMap
@@ -79,53 +52,101 @@ impl KSyms {
             let addr = u64::from_str_radix(parts[0], 16)
                 .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, line.clone()))?;
 
-            btree.insert(addr);
-
-            match name {
-                "net_rx_action"                   => &mut syms.net_rx_action,
-                "__napi_poll"                     => &mut syms.__napi_poll,
-                "netif_receive_skb"               => &mut syms.netif_receive_skb,
-                "netif_receive_skb_core"          => &mut syms.netif_receive_skb_core,
-                "netif_receive_skb_list_internal" => &mut syms.netif_receive_skb_list_internal,
-                "napi_gro_receive"                => &mut syms.napi_gro_receive,
-                "do_xdp_generic"                  => &mut syms.do_xdp_generic,
-                "tcf_classify"                    => &mut syms.tcf_classify,
-                "br_handle_frame"                 => &mut syms.br_handle_frame,
-                "ip_forward"                      => &mut syms.ip_forward,
-                "ip6_forward"                     => &mut syms.ip6_forward,
-                "ip_local_deliver"                => &mut syms.ip_local_deliver,
-                "ip6_input"                       => &mut syms.ip6_input,
-                "ip_rcv"                          => &mut syms.ip_rcv,
-                "ipv6_rcv"                        => &mut syms.ipv6_rcv,
-                "ip_rcv_finish"                   => &mut syms.ip_rcv_finish,
-                "ip6_rcv_finish"                  => &mut syms.ip6_rcv_finish,
-                "nf_hook_slow"                    => &mut syms.nf_hook_slow,
-
-                _ => continue
-            }.start = addr;
+            btree.insert(addr, name.to_string());
         }
 
-        // Find range endings
-        Self::find_range_end(&mut syms.net_rx_action, &btree);
-        Self::find_range_end(&mut syms.__napi_poll, &btree);
-        Self::find_range_end(&mut syms.netif_receive_skb, &btree);
-        Self::find_range_end(&mut syms.netif_receive_skb_core, &btree);
-        Self::find_range_end(&mut syms.netif_receive_skb_list_internal, &btree);
-        Self::find_range_end(&mut syms.napi_gro_receive, &btree);
-        Self::find_range_end(&mut syms.do_xdp_generic, &btree);
-        Self::find_range_end(&mut syms.tcf_classify, &btree);
-        Self::find_range_end(&mut syms.br_handle_frame, &btree);
-        Self::find_range_end(&mut syms.ip_forward, &btree);
-        Self::find_range_end(&mut syms.ip6_forward, &btree);
-        Self::find_range_end(&mut syms.ip_local_deliver, &btree);
-        Self::find_range_end(&mut syms.ip6_input, &btree);
-        Self::find_range_end(&mut syms.ip_rcv, &btree);
-        Self::find_range_end(&mut syms.ipv6_rcv, &btree);
-        Self::find_range_end(&mut syms.ip_rcv_finish, &btree);
-        Self::find_range_end(&mut syms.ip6_rcv_finish, &btree);
-        Self::find_range_end(&mut syms.nf_hook_slow, &btree);
+        // Keep only the symbols we're interested in
+        let syms = btree
+            .iter()
+            .filter_map(|(&range_start, name)| {
+                match name.as_str() {
+                    "net_rx_action" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, _, _| Some(&mut cnt.net_rx_action)
+                    )),
+                    "__napi_poll" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, _, _| Some(&mut cnt.__napi_poll)
+                    )),
+                    "netif_receive_skb" | "netif_receive_skb_core" | "netif_receive_skb_list_internal" | "napi_gro_receive" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            cnt.nf_netdev_ingress = cnt.nf_netdev_ingress.max(std::mem::take(in_nf_hook));
+                            Some(&mut cnt.netif_receive_skb)
+                        }
+                    )),
+                    "do_xdp_generic" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, _, _| Some(&mut cnt.do_xdp_generic)
+                    )),
+                    "tcf_classify" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, _, _| Some(&mut cnt.tcf_classify)
+                    )),
+                    "br_handle_frame" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            *in_nf_hook = 0;
+                            cnt.netif_receive_skb_sub_br = std::mem::take(&mut cnt.netif_receive_skb);
+                            Some(&mut cnt.br_handle_frame)
+                        }
+                    )),
+                    "ip_forward" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            *in_nf_hook = 0;
+                            Some(&mut cnt.ip_forward)
+                        }
+                    )),
+                    "ip6_forward" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            *in_nf_hook = 0;
+                            Some(&mut cnt.ip6_forward)
+                        }
+                    )),
+                    "ip_local_deliver" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            *in_nf_hook = 0;
+                            Some(&mut cnt.ip_local_deliver)
+                        }
+                    )),
+                    "ip6_input" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, _| {
+                            *in_nf_hook = 0;
+                            Some(&mut cnt.ip6_input)
+                        }
+                    )),
+                    "nf_hook_slow" => Option::<SymbolFun>::Some(Box::new(
+                        |_, in_nf_hook, _| Some(in_nf_hook)
+                    )),
+                    "ip_rcv" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, ip_rcv_finish| {
+                            if *ip_rcv_finish == 0 {
+                                cnt.nf_prerouting_v4 = cnt.nf_prerouting_v4.max(*in_nf_hook);
+                            }
+                            *in_nf_hook = 0;
+                            None
+                        }
+                    )),
+                    "ip6_rcv" => Option::<SymbolFun>::Some(Box::new(
+                        |cnt, in_nf_hook, ip_rcv_finish| {
+                            if *ip_rcv_finish == 0 {
+                                cnt.nf_prerouting_v6 = cnt.nf_prerouting_v6.max(*in_nf_hook);
+                            }
+                            *in_nf_hook = 0;
+                            None
+                        }
+                    )),
+                    "ip_rcv_finish" | "ip6_rcv_finish" => Option::<SymbolFun>::Some(Box::new(
+                        |_, _, ip_rcv_finish| Some(ip_rcv_finish)
+                    )),
 
-        Ok(syms)
+                    _ => None
+                }.map(|fun| (range_start, KSymsVal {
+                    range_end: btree
+                        .range(range_start+1..)
+                        .next()
+                        .map(|(&addr, _)| addr)
+                        .unwrap_or(range_start + 1),
+                    fun
+                }))
+            })
+            .collect();
+
+        Ok(Self { syms })
     }
 }
 
@@ -145,78 +166,17 @@ impl Counts {
             }
 
             // Check for known symbols
-            let cnt = match ip {
-                _ if ksyms.net_rx_action.contains(&ip) => &mut c.net_rx_action,
-                _ if ksyms.__napi_poll.contains(&ip) => &mut c.__napi_poll,
-                _ if [&ksyms.netif_receive_skb, &ksyms.netif_receive_skb_core, &ksyms.netif_receive_skb_list_internal, &ksyms.napi_gro_receive]
-                    .iter().any(|r| r.contains(&ip)) => {
-                        c.nf_netdev_ingress = c.nf_netdev_ingress.max(std::mem::take(&mut in_nf_hook));
-                        &mut c.netif_receive_skb
-                    },
-                _ if ksyms.do_xdp_generic.contains(&ip) => &mut c.do_xdp_generic,
-                _ if ksyms.tcf_classify.contains(&ip) => &mut c.tcf_classify,
-                _ if ksyms.br_handle_frame.contains(&ip) => {
-                    in_nf_hook = 0;
-                    c.netif_receive_skb_sub_br = std::mem::take(&mut c.netif_receive_skb);
-                    &mut c.br_handle_frame
-                },
-                _ if ksyms.ip_forward.contains(&ip) => {
-                    in_nf_hook = 0;
-                    // c.nf_forward_v4 = c.nf_forward_v4.max(std::mem::take(&mut in_nf_hook));
-                    &mut c.ip_forward
-                },
-                _ if ksyms.ip6_forward.contains(&ip) => {
-                    in_nf_hook = 0;
-                    // c.nf_forward_v6 = c.nf_forward_v6.max(std::mem::take(&mut in_nf_hook));
-                    &mut c.ip6_forward
-                },
-                _ if ksyms.ip_local_deliver.contains(&ip) => {
-                    in_nf_hook = 0;
-                    // c.nf_local_in_v4 = c.nf_local_in_v4.max(std::mem::take(&mut in_nf_hook));
-                    &mut c.ip_local_deliver
-                },
-                _ if ksyms.ip6_input.contains(&ip) => {
-                    in_nf_hook = 0;
-                    // c.nf_local_in_v6 = c.nf_local_in_v6.max(std::mem::take(&mut in_nf_hook));
-                    &mut c.ip6_input
-                },
-                _ if ksyms.nf_hook_slow.contains(&ip) => &mut in_nf_hook,
-                _ if ksyms.ip_rcv.contains(&ip) => {
-                    if ip_rcv_finish == 0 {
-                        c.nf_prerouting_v4 = c.nf_prerouting_v4.max(in_nf_hook);
+            if let Some((_, KSymsVal { range_end, fun })) = ksyms
+                .syms
+                .range(..=ip)
+                .next_back() {
+                    if ip < *range_end {
+                        if let Some(cnt) = fun(&mut c, &mut in_nf_hook, &mut ip_rcv_finish) {
+                            *cnt = 1;
+                        }
                     }
-                    in_nf_hook = 0;
-                    continue;
                 }
-                _ if ksyms.ipv6_rcv.contains(&ip) => {
-                    if ip_rcv_finish == 0 {
-                        c.nf_prerouting_v6 = c.nf_prerouting_v6.max(in_nf_hook);
-                    }
-                    in_nf_hook = 0;
-                    continue;
-                }
-                _ if ksyms.ip_rcv_finish.contains(&ip) || ksyms.ip6_rcv_finish.contains(&ip) => {
-                    &mut ip_rcv_finish
-                }
-                
-                _ => continue
-            };
-
-            *cnt = 1;
         }
-
-        // DEBUG: output stack trace
-        // if c.ip_local_deliver != 0 {
-        //     use std::io::Write;
-        //     let mut f = File::create("stack_trace").unwrap();
-        //     for frame_idx in 0..max_frames {
-        //         let ip = trace_ptr.add(frame_idx).read_volatile();
-        //         if ip == 0 {
-        //             break;
-        //         }
-        //         writeln!(&mut f, "{ip}").unwrap();
-        //     }
-        // }
 
         *self += c;
     }
